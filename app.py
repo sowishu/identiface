4acf0dacc633cac727239fce75531e7b3fb2a187
sowishu <50516464+sowishu@users.noreply.github.com>
2020-03-11 03:27:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,110 @@
-from flask import Flask, render_template, request
+import glob
+import urllib
+import base64
 
-from flaskext.mysql import MySQL
+import face_recognition
+from flask import Flask, request, render_template
 
-mysql=MySQL()
+from dbop import Profile, ProfileManager
+from binascii import a2b_base64
+import base64
+import datetime
+import time
+import calendar
+import cv2
+from cv2 import imread
+import os
+
+
+
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']='root'
-app.config['MYSQL_DATABASE_DB']='my_db'
-app.config['MYSQL_DATABASE_HOST']='localhost'
-mysql.init_app(app)
-
-@app.route('/')
-def my_form():
-    return render_template('exa.html')
-@app.route('/', methods=["POST"])
-def Authenticate():
-    name=request.form['name']
-    lastname = request.form['lastname']
-    gender=request.form['gender']
-    age = request.form['age']
-    address = request.form['address']
-    phone = request.form['phone']
-    aadhar = request.form['aadhar']
-    mail = request.form['mail']
+#def student():!pip install cmake dlib face_recognition numpy opencv-python
 
 
-    cursor=mysql.connect().cursor()
-    cursor.execute("INSERT INTO register(name,lastname,gender, age, address, phone, aadhar, mail ) VALUES ('"+name+"','"+lastname+"','"+gender+"', "+age+",'"+address+"',"+phone+",'"+aadhar+"','"+mail+"' ")
-    
+@app.route('/dataentry',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      name = result.get('name')
+      lastname=result.get('lastname')
+      gender=result.get('gender')
+      age=result.get('age')
+      address=result.get('address')
+      phone=result.get('phone')
+      aadhar= result.get('aadhar')
+      mail=result.get('mail')
+      imgUrl = result.get('img')
 
 
+      up = urllib.parse.urlparse(imgUrl)
+      head, data = up.path.split(',', 1)
+      bits = head.split(';')
+      mime_type = bits[0] if bits[0] else 'text/plain'
+      charset, b64 = 'ASCII', False
+      for bit in bits:
+         if bit.startswith('charset='):
+            charset = bit[8:]
+         elif bit == 'base64':
+            b64 = True
+
+      # Do something smart with charset and b64 instead of assuming
+      #plaindata = data.decode("base64")
+      plaindata = base64.b64decode(data)
+
+      epoch = time.time()
+      print(epoch)
+      e2 = calendar.timegm(time.gmtime())
+      e1=str(e2)+".jpg"
+      print("Current Image saved :"+e1)
+
+      # Do something smart with mime_type
+      with open(e1, 'wb') as f:
+         f.write(plaindata)
+
+      p=Profile(e2,name,lastname,gender,age,address,phone, aadhar,mail);
+      pm=ProfileManager();
+      pm.createProfile(p);
+   return render_template('DataEntry.html')
+
+@app.route('/fetch',methods = ['POST', 'GET'])
+def fetch():
+   if request.method == 'POST':
+      result = request.form
+      imgUrl = result.get('img')
+
+      up = urllib.parse.urlparse(imgUrl)
+      head, data = up.path.split(',', 1)
+      bits = head.split(';')
+      mime_type = bits[0] if bits[0] else 'text/plain'
+      charset, b64 = 'ASCII', False
+      for bit in bits:
+         if bit.startswith('charset='):
+            charset = bit[8:]
+         elif bit == 'base64':
+            b64 = True
+
+      plaindata = base64.b64decode(data)
+      with open("tmp.jpg", 'wb') as f:
+         f.write(plaindata)
+
+      curr_img = face_recognition.face_encodings(face_recognition.load_image_file("tmp.jpg"))[0]
+      matchingFile = ""
+      for file in glob.glob("*.jpg"):
+        if(file != "tmp.jpg" and matchingFile == ""):
+          dir_img = face_recognition.face_encodings(face_recognition.load_image_file(file))[0]
+          img_result = face_recognition.compare_faces([dir_img], curr_img)
+          print("Compare " + file + " and tmp.jpg")
+          print(img_result)
+          if(img_result == [True]):
+             matchingFile = file
+
+   id,txt = matchingFile.split('.',1)
+   print("Matching file : "+matchingFile)
+   p=Profile(id,'','','','','','','','');
+   pm=ProfileManager();
+   row=pm.select(p);
+   return render_template('DataResult.html',Rname=row[0],Rlastname=row[1],Rgender=row[2],Rage=row[3],Raddress=row[4],Rphone=row[5],Raadhar=row[6],Rmail=row[7])
 
 
 if __name__ == '__main__':
-    app.run()
+   app.run(debug = True)
--- dbop.py
+++ dbop.py
@@ -1,47 +1,36 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
-import app
-from app import result
-app = Flask(__name__)
+import mysql.connector
 
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'MyDB'
-mysql = MySQL(app)
+class Profile:
+    def __init__(self, id,name,lastname,gender,age,address,phone,aadhar, mail):
+     print("test")
+     self.name = name;
+     self.lastname=lastname;
+     self.gender=gender;
+     self.age=age;
+     self.address=address;
+     self.phone=phone;
+     self.aadhar=aadhar;
+     self.mail=mail;
+     self.id=id;
 
-@app.route('/')
-def student():
-   return render_template('DataEntry.html')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def result():
-    if request.method == "POST":
-        result = request.form
-        name=result.get('name')
-
-    class Profile:
-        def __init__(self, name):
-            self.name = name
-
-    return render_template('DataEntry.html')
 
 
 class ProfileManager:
-
     def __init__(self):
-        print("welcome")
+        print("welcome");
 
     def createProfile(self, p):
+
         self.connection = self.__open()
         self.cursor = self.connection.cursor()
-        sql = "INSERT INTO summa(name)VALUES('{}')".format(
-            p.name)
+        sql = "INSERT INTO profile(id,name, lastname,gender, age, address, phone, aadhar, mail)" \
+              "VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}')"\
+            .format(p.id,p.name,p.lastname, p.gender, p.age, p.address,p.phone, p.aadhar, p.mail)
         print("sql:" + sql)
         self.cursor.execute(sql);
         self.connection.commit();
+        self.connection = self.__close();
 
     def updateProfile(self, age, mail):
         self.connection = self.__open()
@@ -61,6 +50,21 @@ class ProfileManager:
         self.connection.commit();
         self.connection = self.__close()
 
+    def select(self,p):
+        self.connection=self.__open();
+        self.cursor=self.connection.cursor();
+        sql="SELECT name ,lastname,gender,age,address,phone, aadhar,mail from profile where id='{}'".format(p.id);
+        print(sql)
+        self.cursor.execute(sql)
+        row=self.cursor.fetchone()
+
+        self.connection = self.__close()
+        return row
+
+    #'sowndarya', 'murali', 'None', '23', '115A, east chitra', '9876543210', '54678934658712', 'sow@gmail.com'
+
+
+
     def __open(self):
         print("connection opened")
         self.connection = mysql.connector.connect(host='127.0.0.1', user='root', passwd='root', database='chennai')
@@ -74,18 +78,9 @@ class ProfileManager:
         print("connection closed")
         self.connection.close()
 
-
-#p = Profile(name)
-# p1=Profile("dhivya","T","female",34,"first new street",9999999999,"77878787","jjj@hotmail.com")
+#p=Profile('ishu')
+#p=Profile("dhivya","T","female",34,"first new street",9999999999,"77878787","jjj@hotmail.com")
 #manager = ProfileManager();
 #manager.createProfile(p)
 # manager.deleteProfile("ishu@gmail.com");
 # manager.updateProfile(55,"sivahari@gmail.com");
-
-
-
-
-
-
-if __name__ == '__main__':
-    app.run()
